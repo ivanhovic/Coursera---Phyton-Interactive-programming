85a2ed4753eb0474d1adc25314915dcfc1a9c357
ivanhovic <murilloivan@gmail.com>
2013-11-12 03:38:10 +0100

    added link, new name

--- Rock-paper-scissors-lizard-Spock.py
+++ Rock-paper-scissors-lizard-Spock.py
@@ -1,4 +1,6 @@
+#IVAN
 # Rock-paper-scissors-lizard-Spock template
+# try code here: http://www.codeskulptor.org/#user24_L58PGa0tBvnsNaL_5.py
 
 
 # The key idea of this program is to equate the strings
@@ -16,8 +18,6 @@ import random
 def number_to_name(number):
     # fill in your code below
     # convert number to a name using if/elif/else
-    # don't forget to return the result!
-    
     if number == 0:
         name = "rock"
         return name
@@ -34,14 +34,12 @@ def number_to_name(number):
         name = "scissors"
         return name
     else: print "you need to imput a number between 0-4"
-    
+# don't forget to return the result!
 
-    
+
 def name_to_number(name):
     # fill in your code below
     # convert name to number using if/elif/else
-    # don't forget to return the result!
-    
     if name == "rock":
         number = 0
         return number
@@ -58,33 +56,34 @@ def name_to_number(name):
         number = 4
         return number
     else: print "you need to imput a correct string"
-    
+
+# don't forget to return the result!
 
 
-def rpsls(name): 
+def rpsls(name):
     # fill in your code below
     # convert name to player_number using name_to_number
-    
     print "Player chooses", name
     number = name_to_number(name)
-
+    
     # compute random guess for comp_number using random.randrange()
-    # convert comp_number to name using number_to_name
     comp_number = random.randrange(0,5,1)
     print "Computer chooses", number_to_name(comp_number)
     
     # compute difference of player_number and comp_number modulo five
     # use if/elif/else to determine winner
     if number == comp_number:
-        print "Player and computer TIE!"
-    elif (number - comp_number) %5 <= 2:
-        print "PLAYER WINS!!"   
+        print "Player and computer tie!"
+    elif (number - comp_number)%5<=2:
+        print "PLAYER WINS!!"
     else: print "COMPUTER WINS!!"
 
+    # convert comp_number to name using number_to_name
     # print results
     print ""
-    
-         
+
+
+
 # test your code
 rpsls("rock")
 rpsls("Spock")
@@ -93,5 +92,4 @@ rpsls("lizard")
 rpsls("scissors")
 
 # always remember to check your completed program against the grading rubric
-
 
